741bcee2f283cfecd11d2ffe86c1d53e01bd3554
Jens Vagelpohl <jens@plyp.com>
2023-02-01 10:04:11 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Products.ZODBMountPoint',
-    version='2.0',
+    version='2.1.dev0',
     url='https://github.com/zopefoundation/Products.ZODBMountPoint',
     project_urls={
         'Issue Tracker': ('https://github.com/zopefoundation/'
